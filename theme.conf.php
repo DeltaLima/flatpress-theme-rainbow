a9d9385c87c41d6b2c7fbc6a72bef4abebb0d382
Marcus <marcus@deltalima.org>
2023-06-26 22:52:08 +0200

    bumped to 0.9.1, changed header

--- theme.conf.php
+++ theme.conf.php
@@ -2,19 +2,19 @@
 
 /*  
 Theme Name: Rainbow Theme
-Original Author: Stealth Settings http://www.stealthsettings.com (ported to FP by Marcoverga, modified tp Rainbow Theme by DeltaLima in 2023)
+Original Author: Stealth Settings http://www.stealthsettings.com (ported to FP by Marcoverga, modified to Rainbow Theme by DeltaLima in 2023)
 Author URI: https://git.la10cy.net/DeltaLima/flatpress-theme-rainbow
 Description: 2-column theme, with orange/transparent look and widget bar on the left with colorful background (stolen from https://websitesetup.org/bootstrap-tutorial-for-beginners/)
 Version: 0.9
-FP Version Author: Marcoverga
-FP Version URI: http://marcoverga86.netsons.org
+FP Version Author: DeltaLima
+FP Version URI: https://deltalima.org
 Forked by DeltaLima in 2023 to "Rainbow Theme"
 */
 
 	$theme['name'] = 'Rainbow Theme';
 	$theme['author'] = 'DeltaLima';
 	$theme['www'] = 'https://deltalima.org';
-	$theme['version'] = 0.9;
+	$theme['version'] = '0.9.1';
 	$theme['style_def'] = 'style.css';
 	$theme['style_admin'] = 'admin.css';
 	

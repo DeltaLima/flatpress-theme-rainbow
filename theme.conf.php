f4cf82067b4cf8fa3fbe67a2b85ba25f31d7cd8b
Marcus <marcus@deltalima.org>
2023-06-26 20:02:38 +0200

    link to git repo makes more sense

--- theme.conf.php
+++ theme.conf.php
@@ -3,7 +3,7 @@
 /*  
 Theme Name: Rainbow Theme
 Original Author: Stealth Settings http://www.stealthsettings.com (ported to FP by Marcoverga, modified tp Rainbow Theme by DeltaLima in 2023)
-Author URI: https://deltalima.org
+Author URI: https://git.la10cy.net/DeltaLima/flatpress-theme-rainbow
 Description: 2-column theme, with orange/transparent look and widget bar on the left with colorful background (stolen from https://websitesetup.org/bootstrap-tutorial-for-beginners/)
 Version: 0.9
 FP Version Author: Marcoverga
